5a9cf35cb0656795c652dfda64f728015ff2fb1f
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2020-01-04 01:15:54 +0700

    Add container uuid.

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ const app = express();
 const host = process.env.HOST;
 const port = process.env.PORT;
 
+import uuidv4 from 'uuid/v4';
+
+const uuid = uuidv4()
+
 // define a route handler for the default home page
 app.get("/", (_, res) => {
-    res.send("Hello world!");
+    res.send(`Hello world from ${uuid}`);
 });
 
 // start the Express server
