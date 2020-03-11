dfe0d9dd9334b7e794b1a31c3844227b41a38ed1
Yosapol Jitrak <yosapoljittarak@hotmail.com>
2020-03-12 02:22:17 +0700

    Add Heavy for HPA.

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,15 @@ app.get("/", (_, res) => {
     res.send(`Hello world from ${uuid}`);
 });
 
+app.get("/heavy", (_, res) => {
+
+    for (let i = 0; i < 1000000; i++) {
+        console.log(`Round: ${i}`);
+    }
+
+    res.send(`Heavy from ${uuid}`);
+});
+
 // start the Express server
 app.listen(port, () => {
     console.log(`server started at http://${host}:${port}`);
